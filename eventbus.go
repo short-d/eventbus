ae1ec3a5611d56240a330864aa811314cf919e18
Harry Liu <byliuyang11@gmail.com>
2019-09-04 00:28:11 -0700

    Clean up code

--- eventbus.go
+++ eventbus.go
@@ -14,47 +14,45 @@ type EventBus struct {
 
 func (e EventBus) Subscribe(eventName string, ch DataChannel) {
 	e.mutex.Lock()
+	defer e.mutex.Unlock()
+
 	if subscribers, ok := e.events[eventName]; ok {
 		e.events[eventName] = append(subscribers, ch)
-		e.mutex.Unlock()
 		return
 	}
 
 	e.events[eventName] = []DataChannel{ch}
-	e.mutex.Unlock()
 }
 
 func (e EventBus) UnSubscribe(eventName string, ch DataChannel) {
 	e.mutex.Lock()
+	defer e.mutex.Unlock()
+
 	subscribers, ok := e.events[eventName]
 	if !ok {
-		e.mutex.Unlock()
 		return
 	}
 
 	for idx, subscriber := range subscribers {
 		if subscriber == ch {
 			e.events[eventName] = append(subscribers[:idx], subscribers[idx+1:]...)
-			e.mutex.Unlock()
 			return
 		}
 	}
-
-	e.mutex.Unlock()
 }
 
 func (e EventBus) Publish(eventName string, data Data) {
 	e.mutex.RLock()
+	defer e.mutex.RUnlock()
+
 	subscribers, ok := e.events[eventName]
 	if !ok {
-		e.mutex.RUnlock()
 		return
 	}
 
 	for _, subscriber := range subscribers {
 		subscriber <- data
 	}
-	e.mutex.RUnlock()
 }
 
 func NewEventBus() EventBus {
